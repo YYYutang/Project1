08d8a068e7fbdd3039f92cb76d618584b66ae684
mirrorsfeng <522764506@qq.com>
2022-10-15 01:17:18 +0800

    fix: conflit fix

--- vite.config.js
+++ vite.config.js
@@ -20,18 +20,10 @@ export default defineConfig({
     }
   },
   server: {
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
     // 代理地址
->>>>>>> 3d8160e (表选择、列选择)
-=======
-    // 代理地址
->>>>>>> c3aee6620285c80bde9626c51042e9bb2965132d
     proxy: {
       '/api': {
-        target: 'http://10.16.11.244:8080',
+        target: 'http://10.16.65.170:8080',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
